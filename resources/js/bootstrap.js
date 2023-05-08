acd9018af1364d292bc8f36100e310c67a7a3129
Armando Ferlanti <armandoferlanti@outlook.it>
2023-05-08 21:27:44 +0200

    collegato correttamente bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import 'bootstrap';
+window._ = _;
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
