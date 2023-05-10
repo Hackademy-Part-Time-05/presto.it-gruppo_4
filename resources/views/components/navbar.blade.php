7d51fd70690144f9f6bd8a3bd385111829e0442e
Giammy88 <conte.gianmarco@yahoo.it>
2023-05-10 17:31:33 +0200

    Inizio Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,41 +1,39 @@
-
-
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+<!-- NAVBAR -->
+<nav id="mainNavbar" class="navbar navbar-expand-lg border-bottom p-0">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-3 fw-bold" href="index.html"><i class="fa-solid fa-cart-shopping me-2"></i>presto.it</a>
+    <button class="navbar-toggler text-presto" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="fa-solid fa-bars"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Chi siamo</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Annunci
+          </a>
+          <ul class="dropdown-menu rounded-0 m-0">
+          <li><a class="dropdown-item" href="annunci.html">Ricerca</a></li>
+            <li><a class="dropdown-item" href="#">Ultimi</a></li>
+          </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contatti</a>
+          </li>S
+          <li class="nav-item">
+          <div class="d-flex">
+            <a href="#" class="nav-link-icon-presto ms-lg-0 ms-2"><i class="fa-solid fa-magnifying-glass"></i></a>
+            <button class="btn btn-presto ms-lg-2 ms-4">Annunci salienti</button>
           </div>
-        </div>
-      </nav>
-
-
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
