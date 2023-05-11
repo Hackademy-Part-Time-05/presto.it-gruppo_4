63659522b87fc48c5c63524ee571cd3a2a0f9d7b
Dennis Sartori <dennis7215@gmail.com>
2023-05-11 21:25:41 +0200

    pagina di base per visualizzazione annuncio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 <x-main>
     prova
 
-    <livewire:create-announcement/>
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="row gx-4 gx-lg-5 align-items-center">
+                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://picsum.photos/600/700" alt="..." /></div>
+                <div class="col-md-6">
+                    <div class="small mb-1">Codice prodotto: inserire un qualcosa che da numeri randomici</div>
+                    <h1 class="display-5 fw-bolder">TITOLO ANNUNCIO</h1>
+                    <div class="fs-5 mb-5">
+                        <span class="text-decoration-line-through"></span>
+                        <span>40,00€</span>
+                    </div>
+                    <p class="lead">Qui ci va la descrizione del prodotto... </p>
+                    <div class="d-flex">
+                        <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
+                        <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                            <i class="bi-cart-fill me-1"></i>
+                            Aggiunge al carrello
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-main>
