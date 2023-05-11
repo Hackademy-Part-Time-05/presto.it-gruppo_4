b477fe031de37794598946bf167dd56ca2bf9f96
ValentinoUccheddu <valentinouccheddu@gmail.com>
2023-05-11 20:36:43 +0200

    cambiato il composer.lock e il gitignore

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,15 +10,15 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Annunci
           </a>
           <ul class="dropdown-menu rounded-0 m-0">
-          <li><a class="dropdown-item" href="annunci.html">Ricerca</a></li>
-            <li><a class="dropdown-item" href="#">Ultimi</a></li>
+          <li><a class="dropdown-item" href="">Ricerca</a></li>
+            <li><a class="dropdown-item" href="">Ultimi</a></li>
           </ul>
           </li>
           <li class="nav-item">
