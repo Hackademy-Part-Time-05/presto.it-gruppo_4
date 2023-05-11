855e7e98ed7fae6f0c25701d15a22ae233af7e1e
Dennis Sartori <dennis7215@gmail.com>
2023-05-11 21:31:36 +0200

    views announcementsShow aggiunta

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -11,14 +11,14 @@
                     <h1 class="display-5 fw-bolder">TITOLO ANNUNCIO</h1>
                     <div class="fs-5 mb-5">
                         <span class="text-decoration-line-through"></span>
-                        <span>$40.00</span>
+                        <span>40,00€</span>
                     </div>
-                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                    <p class="lead">Qui ci va la descrizione del prodotto... </p>
                     <div class="d-flex">
                         <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
                         <button class="btn btn-outline-dark flex-shrink-0" type="button">
                             <i class="bi-cart-fill me-1"></i>
-                            Add to cart
+                            Aggiunge al carrello
                         </button>
                     </div>
                 </div>
