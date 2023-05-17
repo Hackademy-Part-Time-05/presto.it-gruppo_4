75a334bffdbff1c1cd5337fa94c942aecb287ae6
ValentinoUccheddu <valentinouccheddu@gmail.com>
2023-05-17 20:50:57 +0200

    sistemate le notifiche del sito

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -10,21 +10,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body class="container-fluid d-flex flex-column justify-content-between m-0 p-0">
-
-    @if (session()->has('access.denied'))
-    <div class="flex flex-row justify-center my 2 alert alert-danger">
-        {{session('access.denied')}}
-    </div>
-    @endif
-
-    @if (session()->has('message'))
-        <div class="flex flex-row justify-center my 2 alert alert-success">
-            {{session('message')}}
-        </div>
-    @endif
-
         <x-navbar/>
+            @if (session()->has('access.denied'))
+            <div class="flex flex-row justify-center my 2 alert alert-danger">
+                {{session('access.denied')}}
+            </div>
+            @endif
+
+            @if (session()->has('message'))
+                <div class="flex flex-row justify-center my 2 alert alert-success">
+                    {{session('message')}}
+                </div>
+            @endif
+
         {{$slot}}
+        
         <x-footer/>
     @livewireScripts
 </body>
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,11 +1,4 @@
 <div>
-    
-    <div class="container my-5">
-        @if (session()->has('message'))
-        <div class="flex flex-row justify-center my 2 alert alert-success">
-            {{session('message')}}
-        </div>
-    @endif
 
     <form wire:submit.prevent="store">
         @csrf
