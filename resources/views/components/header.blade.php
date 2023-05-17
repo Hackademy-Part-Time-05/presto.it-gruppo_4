a18575d60f2aaef4a96151c34092e338d90df1a7
Armando Ferlanti <armandoferlanti@outlook.it>
2023-05-17 19:53:02 +0200

    modificato footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,14 @@
-    <div class="row flex-grow-1 vh100">
-            <div class="col-12 bg-blue">
-                <h2 class="display-2 text-light position-absolute top-50">
-                    VENDI
-                </h2>
-            </div>
-        </div>
+<div class="container-fluid overflow-hidden">
+  <div class="row flex-grow-1 vh100">
+    <div class="col-12 bg-blue">
+      <h2 class="display-2 text-light position-absolute top-50">
+        VENDI
+      </h2>
     </div>
-    <div class="row flex-grow-1 vh100">
-        <div class="col-12 bg-green">
-            <h2 class="display-2 text-dark position-relative top-50 start-50">PRESTO</h2>
-        </div>
-        </div>
+  </div>
+  <div class="row flex-grow-1 vh100">
+    <div class="col-12 bg-green">
+      <h2 class="display-2 text-dark position-relative top-50 start-50">PRESTO</h2>
     </div>
+  </div>
+</div>
