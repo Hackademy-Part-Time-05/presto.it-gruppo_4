ad62c5381147cc0ef672bdb7923f70af36a489ed
Dennis Sartori <dennis7215@gmail.com>
2023-05-18 23:07:04 +0200

    Aggiunto Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid overflow-hidden">
+{{-- <div class="container-fluid overflow-hidden">
   <div class="row flex-grow-1 vh100">
     <div class="col-12 bg-blue">
       <h2 class="display-2 text-light position-absolute top-50 animate__animated animate__fadeInRightBig">
@@ -12,4 +12,20 @@
       <h2 class="display-2 text-dark position-relative top-50 start-50 animate__animated animate__fadeInLeftBig">PRESTO</h2>
     </div>
   </div>
+</div> --}}
+<header>
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-12 gradient0"  style="height: 400px">
+      <div class="d-flex flex-column align-items-center mt-3">
+        <h2 class="display-2 text-light animate__animated animate__fadeInRightBig">VENDI</h2>
+        <h2 class="display-2 text-dark animate__animated animate__fadeInLeftBig">PRESTO</h2>
+      </div>
+      <div class="text-center mt-4">
+        <p class=" h3 text-dark animate__bounceIn">Vuoi vendere presto e veloce? </p> <p class="h3 text-light animate__bounceIn">Crea il tuo annuncio!</p>
+        <a href="{{route('announcement.create')}}" class="btn btn-lg rounded btnDark border-0 animate__bounceIn">Crea il tuo annuncio</a>
+      </div>
+    </div>
+  </div>
 </div>
+</header>
