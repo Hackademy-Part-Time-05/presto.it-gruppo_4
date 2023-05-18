c6deaaafbb076836ecf88e41dd06d45e1768dc17
Armando Ferlanti <armandoferlanti@outlook.it>
2023-05-18 20:20:19 +0200

    aggiunte animazioni alla home

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,15 @@
 <div class="container-fluid overflow-hidden">
   <div class="row flex-grow-1 vh100">
     <div class="col-12 bg-blue">
-      <h2 class="display-2 text-light position-absolute top-50">
+      <h2 class="display-2 text-light position-absolute top-50 animate__animated animate__fadeInRightBig">
         VENDI
       </h2>
+      
     </div>
   </div>
   <div class="row flex-grow-1 vh100">
     <div class="col-12 bg-green">
-      <h2 class="display-2 text-dark position-relative top-50 start-50">PRESTO</h2>
+      <h2 class="display-2 text-dark position-relative top-50 start-50 animate__animated animate__fadeInLeftBig">PRESTO</h2>
     </div>
   </div>
 </div>
