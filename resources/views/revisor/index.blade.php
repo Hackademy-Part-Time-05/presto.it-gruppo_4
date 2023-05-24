6e1027e8331cfa0f006bd7652fcaf59bc8f126aa
Armando Ferlanti <armandoferlanti@outlook.it>
2023-05-24 20:22:37 +0200

    centrato il front end della view revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,7 +1,7 @@
 <x-main>
     <div class="container-fluid p-5 bg-gradient bg-success p-5 shadow mb-4">
         <div class="row">
-            <div class="col-12 text-light p-5">
+            <div class="col-12 text-light d-flex align-items-center justify-content-center  p-5">
                 <h1 class="display-2">
                     {{$announcement_to_check ? 'Ecco l\'annuncio da revisionare' : 'Non ci sono annunci da revisionare'}}
                 </h1>
@@ -12,53 +12,84 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="https://picsum.photos/1200/400" alt="" class="img-fluid p-3 rounded">
-                            </div>
-
-                            <div class="carousel-item">
-                                <img src="https://picsum.photos/1200/400" alt="" class="img-fluid p-3 rounded">
-                            </div>
-
-                            <div class="carousel-item">
-                                <img src="https://picsum.photos/1200/400" alt="" class="img-fluid p-3 rounded">
-                            </div>
+                    
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      </div>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="<https://picsum.photos/200/300>" class="d-block w-100 carousel-image" alt="...">
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#showCarousel" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
+                        <div class="carousel-item">
+                          <img src="<https://picsum.photos/200/300>" class="d-block w-100 carousel-image" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="<https://picsum.photos/200/300>" class="d-block w-100 carousel-image" alt="...">
+                        </div>
+                      </div>
+                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
                     </div>
-
-                    <h5 class="card-title">Titolo: {{$announcement_to_check->title}}</h5>
-                    <p class="card-text">Descrizione: {{$announcement_to_check->body}}</p>
-                    <p class="card-footer">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
-                </div>
+              </div>
             </div>
 
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <form action="{{route('revisor.accept_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <button type="submit" class="btn btn-success shadow">Accetta</button>
-                    </form>
-                </div>
+                    <!----->
 
-                <div class="col-12 col-md-6">
-                    <form action="{{route('revisor.reject_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
-                    </form>
+
+
+                    <div class="container d-flex align-items-center justify-content-center vh-100">
+                        <div class="text-center">
+                            <h5 class="card-title">Titolo: {{$announcement_to_check->title}}</h5>
+                            <p class="card-text">Descrizione: {{$announcement_to_check->body}}</p>
+                            <p class="card-footer">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
+                        </div>
+                    </div>
+        
+                    <div class="row">
+                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
+                            <form action="{{route('revisor.accept_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn rounded-pill btn-success shadow">Accetta</button>
+                            </form>
+                        </div>
+        
+                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
+                            <form action="{{route('revisor.reject_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn rounded-pill btn-danger  shadow">Rifiuta</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    @endif
+            @endif
+                        </div>
+                      </div>
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                   
 </x-main>
