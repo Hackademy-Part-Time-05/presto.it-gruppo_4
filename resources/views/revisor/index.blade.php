31a25a104b785dd79d4be902539823d0e0a8eacb
Dennis Sartori <dennis7215@gmail.com>
2023-05-30 14:39:07 +0200

    Agiiustato lato front end della zona revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 <x-main>
-    <div class="container-fluid p-5 bg-gradient bg-success p-5 shadow mb-4">
+    <div class="container-fluid p-5 gradient0 p-5 shadow mb-4">
         <div class="row">
             <div class="col-12 text-light d-flex align-items-center justify-content-center  p-5">
                 <h1 class="display-2">
@@ -16,37 +16,15 @@
     @if ($announcement_to_check)
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-md-6">
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         @foreach ($announcement_to_check->images as $key => $image)
                             <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                                <img src="{{$image->getUrl(400,300)}}" class="d-block w-50 carousel-image" alt="...">
+                                <img src="{{$image->getUrl(400,300)}}" class="d-block w-100 carousel-image" alt="...">
                             </div>
                         @endforeach
                     </div>
-                    <div class="col-md-3 border-end">
-                        <h5 class="tc-accent mt-3">Tag</h5>
-                        <div class="p-2">
-                            @if ($image->labels)
-                                @foreach ($image->labels as $label)
-
-                                    <p class="d-inline">{{$label}},</p>
-                                    
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                <div class="col-md-3">
-                    <div class="card-body">
-                        <h5 class="tc-accent">Revisione immagini</h5>
-                        <p>Adulti: <span class="{{ $image->adult }}"></span></p>
-                        <p>Satira: <span class="{{ $image->spoof }}"></span></p>
-                        <p>Medicina: <span class="{{ $image->medical }}"></span></p>
-                        <p>Violenza: <span class="{{ $image->violence }}"></span></p>
-                        <p>Contenuti espliciti: <span class="{{ $image->racy }}"></span></p>
-                    </div>
-                </div>
                     @if (count($announcement_to_check->images) > 1)
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -60,6 +38,28 @@
                     @endif
                 </div>
             </div>
+            <div class="col-md-6 d-flex align-items-center">
+                <div class="card-body mx-3">
+                    <h5 class="tc-accent fw-bold text-dark">Revisione immagini</h5>
+                    <p class="ms-2">Adulti: <span class="{{ $image->adult }}"></span></p>
+                    <p class="ms-2">Satira: <span class="{{ $image->spoof }}"></span></p>
+                    <p class="ms-2">Medicina: <span class="{{ $image->medical }}"></span></p>
+                    <p class="ms-2">Violenza: <span class="{{ $image->violence }}"></span></p>
+                    <p class="ms-2">Contenuti espliciti: <span class="{{ $image->racy }}"></span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 my-3">
+            <h5 class="tc-accent fw-bold text-dark">Tag</h5>
+            <div class="text-dark">
+                @if ($image->labels)
+                    @foreach ($image->labels as $label)
+
+                        <p class="d-inline">{{$label}},</p>
+                        
+                    @endforeach
+                @endif
+            </div>
         </div>
     </div>
 
@@ -68,10 +68,12 @@
 
 
                     <div class="container d-flex align-items-center justify-content-center vh-100">
-                        <div class="text-center">
-                            <h5 class="card-title fs-3 my-3 text-dark"><span class="fw-bold text-uppercase">{{__('ui.title')}}: </span> {{$announcement_to_check->title}}</h5>
-                            <p class="card-text fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.description')}}: </span> {{$announcement_to_check->body}}</p>
-                            <p class="card-footer fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.postedOn')}}: </span> {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
+                        <div class="col-12 col-md-6">
+                            <div class="text-center">
+                                <h5 class="card-title fs-3 my-3 text-dark"><span class="fw-bold text-uppercase">{{__('ui.title')}}: </span> {{$announcement_to_check->title}}</h5>
+                                <p class="card-text fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.description')}}: </span> {{$announcement_to_check->body}}</p>
+                                <p class="card-footer fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.postedOn')}}: </span> {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
+                            </div>
                         </div>
                     </div>
         
@@ -165,20 +167,4 @@
                       </div>                  
                       
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                   
 </x-main>
