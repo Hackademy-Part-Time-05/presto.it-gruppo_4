52d36a8c0e6d53c48f5e2267635971788df09faa
Giammy88 <conte.gianmarco@yahoo.it>
2023-05-31 20:17:43 +0200

    Watermark completo

--- app/Jobs/WatermarkJob.php
+++ app/Jobs/WatermarkJob.php
@@ -41,8 +41,10 @@ class WatermarkJob implements ShouldQueue
 
        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
        $image=SpatieImage::load($srcPath);
-       $image->watermark('resources/img/logo.png') 
-       ->watermarkPosition(Manipulations::POSITION_CENTER);
+       $image->watermark('resources/img/logoWatermark.png') 
+       ->watermarkPosition(Manipulations::POSITION_BOTTOM)
+       ->watermarkPadding(55)
+       ->watermarkOpacity(50);
        $image->save($srcPath);
     }
 }
