80e3c9991d8006cf6fe88de5478ea6e559f3f092
Giuseppe Arcieri <giuseppearcieri@hotmail.it>
2023-06-01 21:44:48 +0200

    revisor show

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -6,15 +6,27 @@
 <x-main>
     <div class="container-fluid p-5 gradient0 p-5 shadow mb-4">
         <div class="row">
-            <div class="col-12 text-light d-flex align-items-center justify-content-center  p-5">
-                <h1 class="display-2">
+            <div class="col-12 text-dark d-flex justify-content-start p-0 m-0">
+                <h1 class="text-uppercase fst-italic fw-bold">
                     {{ $announcement_to_check ?  __('ui.hereIsTheAdvertisementToReview') : __('ui.thereAreNoAdvertisementsToReview') }}
                 </h1>
             </div>
         </div>
     </div>
     @if ($announcement_to_check)
-    <div class="container">
+    <div class="container border">
+        <div class="row d-flex justify-content-between bg-dark p-3 mb-5">
+            <div class="col-4 fs-1 fw-bold text-light shadow">{{__('ui.title')}}: {{$announcement_to_check->title}}</div>
+            <div class="col-4 fs-5 text-light my-auto fst-italic">{{__('ui.postedOn')}}: {{$announcement_to_check->created_at->format('d/m/Y')}}</div>
+            <form class="col-2 my-auto d-flex justify-content-end" action="{{route('revisor.accept_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
+                @csrf @method('PATCH')
+                <button type="button" class="btn rounded-pill btn-success shadow" data-bs-toggle="modal" data-bs-target="#acceptModal">{{__('ui.accept')}}</button>
+            </form>
+            <form class="col-2 my-auto" action="{{route('revisor.reject_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
+                @csrf @method('PATCH')
+                <button type="button" class="btn rounded-pill btn-danger shadow" data-bs-toggle="modal" data-bs-target="#rejectModal">{{__('ui.reject')}}</button>
+            </form>
+        </div>
         <div class="row">
             <div class="col-md-6">
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -38,80 +50,49 @@
                     @endif
                 </div>
             </div>
-            <div class="col-md-6 d-flex align-items-center">
-                <div class="card-body mx-3">
-                    <h5 class="tc-accent fw-bold text-dark">Revisione immagini</h5>
-                    <p class="ms-2">Adulti: <span class="{{ $image->adult }}"></span></p>
-                    <p class="ms-2">Satira: <span class="{{ $image->spoof }}"></span></p>
-                    <p class="ms-2">Medicina: <span class="{{ $image->medical }}"></span></p>
-                    <p class="ms-2">Violenza: <span class="{{ $image->violence }}"></span></p>
-                    <p class="ms-2">Contenuti espliciti: <span class="{{ $image->racy }}"></span></p>
+                <div class="col-md-6">
+                    <div class="row">
+                        <p class="card-text fs-5 my-3 text-break"><span class=" text-dark fw-bold text-uppercase">{{__('ui.description')}}: </span> {{$announcement_to_check->body}}</p>
+                    </div>
+                    <div class="row">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>Adulti:</td>
+                                    <td><span class="{{$image->adult}}"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Satira:</td>
+                                    <td><span class="{{$image->spoof}}"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Medicina:</td>
+                                    <td><span class="{{$image->medical}}"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Violenza:</td>
+                                    <td><span class="{{$image->violence}}"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Contenuti espliciti:</td>
+                                    <td><span class="{{$image->racy}}"></span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <h5 class="tc-accent fw-bold text-dark">Tag</h5>
+                        <div class="text-dark">
+                            @if ($image->labels)
+                                @foreach ($image->labels as $label)
+                                    <p class="fst-italic d-inline">#{{$label}},</p>
+                                @endforeach
+                            @endif            
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-6 my-3">
-            <h5 class="tc-accent fw-bold text-dark">Tag</h5>
-            <div class="text-dark">
-                @if ($image->labels)
-                    @foreach ($image->labels as $label)
-
-                        <p class="d-inline">{{$label}},</p>
-                        
-                    @endforeach
-                @endif
-            </div>
         </div>
     </div>
-
-                    <!----->
-
-
-
-                    <div class="container d-flex align-items-center justify-content-center vh-100">
-                        <div class="col-12 col-md-6">
-                            <div class="text-center">
-                                <h5 class="card-title fs-3 my-3 text-dark"><span class="fw-bold text-uppercase">{{__('ui.title')}}: </span> {{$announcement_to_check->title}}</h5>
-                                <p class="card-text fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.description')}}: </span> {{$announcement_to_check->body}}</p>
-                                <p class="card-footer fs-5 my-3"><span class=" text-dark fw-bold text-uppercase">{{__('ui.postedOn')}}: </span> {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
-                            </div>
-                        </div>
-                    </div>
-        
-                    {{-- <div class="row">
-                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
-                            <form action="{{route('revisor.accept_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="submit" class="btn rounded-pill btn-success shadow">{{__('ui.accept')}}</button>
-                            </form>
-                        </div>
-        
-                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
-                            <form action="{{route('revisor.reject_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="submit" class="btn rounded-pill btn-danger  shadow">{{__('ui.reject')}}</button>
-                            </form>
-                        </div>
-                    </div> --}}
-
-                    <div class="row">
-                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
-                            <form action="{{route('revisor.accept_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="button" class="btn rounded-pill btn-success shadow" data-bs-toggle="modal" data-bs-target="#acceptModal">{{__('ui.accept')}}</button>
-                            </form>
-                        </div>
-                    
-                        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center my-5">
-                            <form action="{{route('revisor.reject_announcement', ['announcement'=> $announcement_to_check])}}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="button" class="btn rounded-pill btn-danger shadow" data-bs-toggle="modal" data-bs-target="#rejectModal">{{__('ui.reject')}}</button>
-                            </form>
-                        </div>
-                    </div>
 
 
     <!-- Modal di conferma per l'accettazione -->
@@ -167,7 +148,7 @@
                       </div>                  
             
 
-<div>
+<div class="container flex-grow-1 my-5">
     <table class="table">
         <tr>
             <th>Titolo</th>
